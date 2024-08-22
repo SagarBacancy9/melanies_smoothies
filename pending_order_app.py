9689733c21dca8fd9d108c4d63deda4ea36b9443
SagarBacancy9 <sagar.maniar@bacancy.com>
2024-08-22 16:01:31 +0530

    Update pending_order_app.py
    Fix Session Issue

--- pending_order_app.py
+++ pending_order_app.py
@@ -7,8 +7,9 @@ from snowflake.snowpark.functions import col, when_matched
 st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
 st.write("Orders That need to be filled.")
 
-session = get_active_session()
-
+# session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 
 my_dataframe = session.table("smoothies.public.orders").filter(col('ORDER_FILLED')==0).collect()
